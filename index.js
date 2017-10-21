b18eeb76becfe189ac080aee1f094ade62504e28
euler16 <nilayshrivastava1729@gmail.com>
2017-10-22 00:12:41 +0530

    added html

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-require('dotenv').load()
+require('dotenv').load();
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready',function(){
+    mainWindow = new BrowserWindow({
+
+    });
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Menu
+const mainMenuTemplate = [
+    {
+        label: 'File'
+    }
+];
